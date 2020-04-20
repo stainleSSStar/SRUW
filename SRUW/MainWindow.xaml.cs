207c85838361280534521982413c738a48809e06
stainleSSStar <stainleSSStar@STAINHOST>
2020-04-20 19:17:02 +0200

    v1.10

---
+++ SRUW/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SRUW
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            String processname = Process.GetCurrentProcess().ProcessName;
+            Process[] prorun = Process.GetProcessesByName(processname);
+            if (prorun.Length > 1)
+            {
+                MessageBox.Show("Aplikacja jest już uruchomiona", "SRUW - Informacja", MessageBoxButton.OK, MessageBoxImage.Information);
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                InitializeComponent();
+            }
+        }
+        private void MW_F_Exit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
